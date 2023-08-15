e8c003fc845d3fb10959e180dcf692086829cd58
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-15 12:46:39 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,10 +32,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST: insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted, new id = " + newSeller.getId());
+		System.out.println("\n=== TEST: Insert Seller ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted, new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST: Update Seller ===");
+		seller = sellerDao.findById(18);
+		seller.setName("Chris Rock");
+		seller.setBaseSalary(6200.0);
+		seller.setEmail("chris@hotmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
