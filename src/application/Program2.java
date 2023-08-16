bba3deb5b557815dc7414698960049f8f1b55cfe
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-16 11:21:26 -0300

    feat: findAll department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,11 +1,11 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 	
@@ -15,7 +15,7 @@ public class Program2 {
 		Scanner scan = new Scanner(System.in);
 		
 //		System.out.println("\n=== TEST: findById Department ===");
-		Department department = departmentDao.findById(5);
+//		Department department = departmentDao.findById(5);
 //		System.out.println(department);
 //		
 //		System.out.println("\n=== TEST: Insert new Department ===");
@@ -23,14 +23,20 @@ public class Program2 {
 //		departmentDao.insert(newDepartment);
 //		System.out.println("Inserted, new id = " + newDepartment.getId());
 		
-		System.out.println("\n=== TEST: Update Department ===");
-		System.out.print("Which department you want to change (Id): ");
-		int id = scan.nextInt();
-		System.out.print("New name: ");
-		String nome = scan.next();
-		department = departmentDao.findById(id);
-		department.setName(nome);
-		departmentDao.update(department);
+//		System.out.println("\n=== TEST: Update Department ===");
+//		System.out.print("Which department you want to change (Id): ");
+//		int id = scan.nextInt();
+//		System.out.print("New name: ");
+//		String nome = scan.next();
+//		department = departmentDao.findById(id);
+//		department.setName(nome);
+//		departmentDao.update(department);
+		
+		System.out.println("\n=== TEST: findAll Department ===");
+		List<Department> list = departmentDao.findAll();
+		for(Department dep: list) {
+			System.out.println(dep);
+		}
 		
 		
 		
