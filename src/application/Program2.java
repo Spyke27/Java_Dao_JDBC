58fdb5693670a2362b484f789309a6a145380d31
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-16 11:07:49 -0300

    feat: findById department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,21 +14,23 @@ public class Program2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+//		System.out.println("\n=== TEST: findById Department ===");
+		Department department = departmentDao.findById(5);
+//		System.out.println(department);
+//		
 //		System.out.println("\n=== TEST: Insert new Department ===");
 //		Department newDepartment = new Department(5, "Developer");
 //		departmentDao.insert(newDepartment);
 //		System.out.println("Inserted, new id = " + newDepartment.getId());
-//		
-//		System.out.println("\n=== TEST: Update Department ===");
-//		System.out.print("Which department you want to change (Id): ");
-//		int id = scan.nextInt();
-//		System.out.print("New name: ");
-//		String nome = scan.next();
-//		Department department = departmentDao.findById(id);
 		
-		System.out.println("\n=== TEST: findById Department ===");
-		Department department = departmentDao.findById(5);
-		System.out.println(department);
+		System.out.println("\n=== TEST: Update Department ===");
+		System.out.print("Which department you want to change (Id): ");
+		int id = scan.nextInt();
+		System.out.print("New name: ");
+		String nome = scan.next();
+		department = departmentDao.findById(id);
+		department.setName(nome);
+		departmentDao.update(department);
 		
 		
 		
