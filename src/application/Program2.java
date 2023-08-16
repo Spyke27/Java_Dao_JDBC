49f8b9c6afcba6e54435ada1c4493b7222d002e5
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-16 10:51:39 -0300

    findById department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,25 +1,38 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 	
 	static DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
-		Department newDepartment = new Department(5, "Developer");
-		departmentDao.insert(newDepartment);
-		System.out.println("\n=== TEST: Insert new Department ===");
-		System.out.println("Inserted, new id = " + newDepartment.getId());
+//		System.out.println("\n=== TEST: Insert new Department ===");
+//		Department newDepartment = new Department(5, "Developer");
+//		departmentDao.insert(newDepartment);
+//		System.out.println("Inserted, new id = " + newDepartment.getId());
+//		
+//		System.out.println("\n=== TEST: Update Department ===");
+//		System.out.print("Which department you want to change (Id): ");
+//		int id = scan.nextInt();
+//		System.out.print("New name: ");
+//		String nome = scan.next();
+//		Department department = departmentDao.findById(id);
+		
+		System.out.println("\n=== TEST: findById Department ===");
+		Department department = departmentDao.findById(5);
+		System.out.println(department);
 		
 		
 		
-		
-		
-
+		scan.close();
 	}
 
 }
